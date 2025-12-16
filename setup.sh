ce819578eee98c87eb6d0e4e9eb8cb79f06bb7bd
Daniel <daniel@eylo.nl>
2025-12-16 10:15:31 +0100

    -

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 e2fsck -f -y /dev/sda1
-
 resize2fs /dev/sda1 99G
 
 echo Yes | parted ---pretend-input-tty /dev/sda resizepart 1 100GiB
@@ -11,38 +10,94 @@ partprobe /dev/sda
 
 pvcreate /dev/sda2
 vgcreate vg0 /dev/sda2
-lvcreate -L 10G -n root vg0
-# lvcreate -L 4G -n swap vg0
-mkfs.ext4 /dev/vg0/root
-# mkswap /dev/vg0/swap
 
+lvcreate -L 10G -n root vg0
+mkfs.ext4 /dev/vg0/root
+
+lvcreate -L 5G -n home vg0
+mkfs.ext4 /dev/vg0/home
+
+lvcreate -L 10G -n var vg0
+mkfs.ext4 /dev/vg0/var
+
+lvcreate -L 5G -n var-tmp vg0
+mkfs.ext4 /dev/vg0/var-tmp
+
+lvcreate -L 5G -n var-log vg0
+mkfs.ext4 /dev/vg0/var-log
+
+lvcreate -L 5G -n var-log-audit vg0
+mkfs.ext4 /dev/vg0/var-log-audit
+
+lvcreate -L 4G -n swap vg0
+mkswap /dev/vg0/swap
+
+# Mount LVs
 mkdir -p /mnt/new
 mount /dev/vg0/root /mnt/new
 
+mkdir -p /mnt/new/var
+mount /dev/vg0/var /mnt/new/var
+
+mkdir -p /mnt/new/var/log
+mount /dev/vg0/var-log /mnt/new/var/log
+
+mkdir -p /mnt/new/var/tmp
+
+# Mount old filesystem
 mkdir -p /mnt/old
 mount /dev/sda1 /mnt/old
 
-rsync -aHAX --numeric-ids --info=progress2 --exclude={"/dev/*","/proc/*","/sys/*","/run/*","/tmp/*","/mnt/*","/media/*","/lost+found"} /mnt/old/ /mnt/new/
+# Copy files
+rsync -aHAX --numeric-ids --info=progress2 --exclude={"/home/*","/dev/*","/proc/*","/sys/*","/run/*","/tmp/*","/mnt/*","/media/*","/lost+found"} /mnt/old/ /mnt/new/
 
-umount -l /mnt/old /mnt/new
+umount -l /mnt/old /mnt/new/var/log /mnt/new/var /mnt/new
 pvcreate -y /dev/sda1
 vgextend vg0 /dev/sda1
 pvmove /dev/sda2 /dev/sda1
 vgreduce vg0 /dev/sda2
 pvremove /dev/sda2
 
+parted ---pretend-input-tty /dev/sda rm 2
+parted ---pretend-input-tty /dev/sda set 1 lvm on
+parted ---pretend-input-tty /dev/sda resizepart 1 100%
+partprobe /dev/sda
+pvresize /dev/sda1
+
 mount /dev/vg0/root /mnt/new
+mount /dev/vg0/var /mnt/new/var
+mount /dev/vg0/var-log /mnt/new/var/log
+mount /dev/vg0/var-tmp /mnt/new/var/tmp
 
 for d in dev proc sys run; do mount --bind /$d /mnt/new/$d; done
 chroot /mnt/new /bin/bash -x <<'EOF'
   dnf install -y lvm2 grub2 grubby
   
-  UUID_ROOT=$(blkid -s UUID -o value /dev/vg0/root)
-  echo "UUID=$UUID_ROOT / ext4 defaults 0 1" > /etc/fstab
-  
+  BLKID_ROOT=$(blkid -s UUID -o value /dev/vg0/root)
+  echo "UUID=$BLKID_ROOT / ext4 defaults,rw,relatime 0 0" > /etc/fstab
+
+  BLKID=$(blkid -s UUID -o value /dev/vg0/home)
+  echo "UUID=$BLKID /home ext4 defaults,rw,nosuid,nodev,relatime 0 0" >> /etc/fstab
+
+  BLKID=$(blkid -s UUID -o value /dev/vg0/var)
+  echo "UUID=$BLKID /var ext4 defaults,rw,nosuid,nodev,relatime 0 0" >> /etc/fstab
+
+  BLKID=$(blkid -s UUID -o value /dev/vg0/var-tmp)
+  echo "UUID=$BLKID /var/tmp ext4 defaults,rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
+
+  BLKID=$(blkid -s UUID -o value /dev/vg0/var-log)
+  echo "UUID=$BLKID /var/log ext4 defaults,rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
+
+  BLKID=$(blkid -s UUID -o value /dev/vg0/var-log-audit)
+  echo "UUID=$BLKID /var/log/audit ext4 defaults,rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
+
+  echo "tmpfs /tmp tmpfs defaults,rw,nosuid,nodev,noexec,relatime,size=2G 0 0" >> /etc/fstab
+
+  echo "tmpfs /dev/shm tmpfs defaults,rw,nosuid,nodev,noexec,relatime,size=2G 0 0" >> /etc/fstab
+
   grubby --update-kernel=ALL --remove-args="root=/dev/sda1"
-  grubby --update-kernel=ALL --remove-args="root=UUID=$UUID_ROOT"
-  grubby --update-kernel=ALL --args="root=UUID=$UUID_ROOT rd.lvm.lv=vg0/root"
+  grubby --update-kernel=ALL --remove-args="root=UUID=$BLKID_ROOT"
+  grubby --update-kernel=ALL --args="root=UUID=$BLKID_ROOT rd.lvm.lv=vg0/root"
   
   dracut -f --regenerate-all
   
